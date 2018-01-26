2ce2766a5bb1c63606fdd0224a02c4148edb0056
angelazhou <angelazhou@ct-accel.com>
2018-01-26 20:20:42 +0800

    add firebase config

---
+++ src/config/firebase/config.js
@@ -0,0 +1,8 @@
+const config = {
+ 'authDomain' : 'todo-list-81393.firebaseapp.com',
+ 'apiKey' : 'AIzaSyCWeGOIufSPv14-q7aH37Zd1aSKqPTMnDc',
+ 'databaseURL' : 'https://todo-list-81393.firebaseio.com/',
+ 'serviceAccount' : require("./todo-list-81393-firebase-adminsdk-86dct-3a05830dba.json")
+}
+module.exports = config;
+
